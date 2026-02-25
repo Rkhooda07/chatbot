e36a215548ba73dd77d3ea511e9d356938e03c04
Rakshit Hooda <rakshit.official703@gmail.com>
2026-02-25 17:16:13 +0530

    5h: add chatbot responses using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ChatInput } from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
 import './App.css';
+import { Chatbot } from 'supersimpledev';
 
 function App() {
   // Destructuring array ->
@@ -10,6 +11,15 @@ function App() {
   // const [chatMessages, setChatMessages] = array;
   // const chatMessages = array[0];
   // const setChatMessages = array[1];
+
+  useEffect(() => {
+    Chatbot.addResponses({
+      "sup": "fine wbu",
+      "give me a unique id": function() {
+        return `Sure! Here's a unique id -> ${crypto.randomUUID()}`;
+      }
+    });
+  }, []);
 
   return (
     <div className= "app-container">
